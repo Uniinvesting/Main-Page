32e5c058dae5d5b17ea169347dfb8bfe3dcf7643
mariyatur27 <63456223+mariyatur27@users.noreply.github.com>
2022-08-20 16:11:58 -0400

    final touches on the footer

--- uniinvesting_main_page/src/components/scroll-section-one.js
+++ uniinvesting_main_page/src/components/scroll-section-one.js
@@ -4,7 +4,7 @@ import {investing_scroll} from './data/scroll_one'
 export default function Scroll_section_one() {
   return (
     <div className='scroll-section'>
-        <a href=''>
+        <a href='' className='scroll-a'>
             <div className='article-box section-box'>
                 <h1>Investing</h1>
                 <h3>Explore</h3>
@@ -12,7 +12,7 @@ export default function Scroll_section_one() {
         </a>
         {investing_scroll.map((data, key) => {
             return (
-                <a href={data.link}>
+                <a href={data.link} className='scroll-a'>
                     <div className='article-box' id={data.id} key={key}>
                         <h2>{data.name}</h2>
                         <h6>By: {data.author}</h6>
--- uniinvesting_main_page/src/components/scroll-section-three.js
+++ uniinvesting_main_page/src/components/scroll-section-three.js
@@ -4,7 +4,7 @@ import {crypto_scroll} from './data/scroll_three'
 export default function Scroll_section_three() {
   return (
     <div className='scroll-section'>
-        <a href=''>
+        <a href='' className='scroll-a'>
             <div className='article-box section-box'>
                 <h1>Crypto</h1>
                 <h3>Explore</h3>
@@ -12,7 +12,7 @@ export default function Scroll_section_three() {
         </a>
         {crypto_scroll.map((data, key) => {
             return (
-                <a href={data.link}>
+                <a href={data.link} className='scroll-a'>
                     <div className='article-box' id={data.id} key={key}>
                         <h2>{data.name}</h2>
                         <h6>By: {data.author}</h6>
