52031da1d8437d9bd5997100002549f4357661c2
PilotPrix <69162332+PilotPrix@users.noreply.github.com>
2022-08-20 18:58:25 -0400

    Merge branch 'main' of https://github.com/Uniinvesting/Main-Page into main

--- uniinvesting_main_page/src/components/data/scroll_one.js
+++ uniinvesting_main_page/src/components/data/scroll_one.js
@@ -1,4 +1,31 @@
-export const investing_scroll = [
+
+// Cohere
+const cohere = require('cohere-ai');
+cohere.init('UN4toLPSBP9nsovrtmfO5M8u1X8nYDV3NdaWNgA5');
+
+async function parse() {
+    const response = await cohere.generate({
+        model: 'xlarge',
+        prompt: 'Buying a stock is easy, but buying the right stock without a time-tested strategy is incredibly hard. So what are the best stocks to buy now or put on a watchlist?\n\nTLDR:',
+        max_tokens: 10,
+        temperature: 0.5,
+        k: 0,
+        p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+        stop_sequences: ["--"],
+        return_likelihoods: 'NONE'
+      })
+    return response.body.generations[0].text
+}
+
+async function start() {
+    const wait = await parse()
+    return wait
+}
+
+// Data to load
+let investing_scroll = [
     {
         name: 'lorem ipsum',
         link: '',
@@ -11,7 +38,7 @@ export const investing_scroll = [
         id: 'invest-article-2',
         author: 'Andrian Polak',
         link: '',
-        description: "Want to invest like a pro? Learn the basics of investing from us and we'll have you on the road to investing in no time."
+        description: ""
     },
     {
         name: 'lorem ipsum',
@@ -35,3 +62,7 @@ export const investing_scroll = [
         description: "Want to invest like a pro? Learn the basics of investing from us and we'll have you on the road to investing in no time."
     }
 ]
+
+(async () => {
+    investing_scroll[1]["description"] = await start();
+})()
--- uniinvesting_main_page/src/components/web_scraper.js
+++ uniinvesting_main_page/src/components/web_scraper.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import puppeteer from "puppeteer"
+// import puppeteer from "puppeteer"
 
-async function start() {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    await page.goto("https://learnwebcode.github.io/practice-requests/")
-    await browser.close()
-}
+// async function start() {
+//     const browser = await puppeteer.launch()
+//     const page = await browser.newPage()
+//     await page.goto("https://learnwebcode.github.io/practice-requests/")
+//     await browser.close()
+// }
 
 export default function web_scraper() {
-    start()
+    // start()
     return (
         <div>
             Hello world
