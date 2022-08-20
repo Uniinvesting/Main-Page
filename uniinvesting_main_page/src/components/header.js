c7fe9e158ee30e7375a02d2a14c5e3b50136aa8c
mariyatur27 <63456223+mariyatur27@users.noreply.github.com>
2022-08-20 11:50:54 -0400

    finished the first scrolling category and header

---
+++ uniinvesting_main_page/src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div id='header-section'>
+        <div className='centered-text'>
+            <h1>Welcome to Finance Platform @ Uni Investing</h1>
+        </div>
+
+        <div className='centered-text'>
+            <p>Personal finance is the art of managing your money. How much to save, where to invest and how to budget. None of us are born knowing how to put together a smart, simple financial plan. That’s why we’re here.</p>
+        </div>
+    </div>
+  )
+}
---
+++ uniinvesting_main_page/src/components/scroll-section-one.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {investing_scroll} from './data/scroll_one'
+
+export default function Scroll_section_one() {
+  return (
+    <div className='scroll-section'>
+        <a href=''>
+            <div className='article-box section-box'>
+                <h1>Investing</h1>
+                <h3>Explore</h3>
+            </div>
+        </a>
+        {investing_scroll.map((data, key) => {
+            return (
+                <a href=''>
+                    <div className='article-box' id={data.id} key={key}>
+                        <h2>{data.name}</h2>
+                        <h6>By: {data.author}</h6>
+                        <p>{data.description}</p>
+                    </div>
+                </a>
+            )
+        })}
+    </div>
+  )
+}
