f790c8b34487c3f2fabee13b92d094228b20289f
PilotPrix <69162332+PilotPrix@users.noreply.github.com>
2022-08-20 20:31:16 -0400

    Update

--- uniinvesting_main_page/src/components/cohere.js
+++ uniinvesting_main_page/src/components/cohere.js
@@ -1,27 +1,27 @@
-const cohere = require('cohere-ai');
-cohere.init('UN4toLPSBP9nsovrtmfO5M8u1X8nYDV3NdaWNgA5');
+// const cohere = require('cohere-ai');
+// cohere.init('UN4toLPSBP9nsovrtmfO5M8u1X8nYDV3NdaWNgA5');
 
-async function parse() {
-    const response = await cohere.generate({
-        model: 'xlarge',
-        prompt: 'Buying a stock is easy, but buying the right stock without a time-tested strategy is incredibly hard. So what are the best stocks to buy now or put on a watchlist?\n\nTLDR:',
-        max_tokens: 10,
-        temperature: 0.5,
-        k: 0,
-        p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-        stop_sequences: ["--"],
-        return_likelihoods: 'NONE'
-      })
-    return response.body.generations[0].text
-}
+// async function parse() {
+//     const response = await cohere.generate({
+//         model: 'xlarge',
+//         prompt: 'Buying a stock is easy, but buying the right stock without a time-tested strategy is incredibly hard. So what are the best stocks to buy now or put on a watchlist?\n\nTLDR:',
+//         max_tokens: 10,
+//         temperature: 0.5,
+//         k: 0,
+//         p: 1,
+//         frequency_penalty: 0,
+//         presence_penalty: 0,
+//         stop_sequences: ["--"],
+//         return_likelihoods: 'NONE'
+//       })
+//     return response.body.generations[0].text
+// }
 
-async function start() {
-    const wait = await parse()
-    return wait
-}
+// async function start() {
+//     const wait = await parse()
+//     return wait
+// }
 
-(async () => {
-    console.log(await start())
-})()
+// (async () => {
+//     console.log(await start())
+// })()
--- uniinvesting_main_page/src/components/data/scroll_one.js
+++ uniinvesting_main_page/src/components/data/scroll_one.js
@@ -1,28 +1,28 @@
 
 // Cohere
-const cohere = require('cohere-ai');
-cohere.init('UN4toLPSBP9nsovrtmfO5M8u1X8nYDV3NdaWNgA5');
+// const cohere = require('cohere-ai');
+// cohere.init('UN4toLPSBP9nsovrtmfO5M8u1X8nYDV3NdaWNgA5');
 
-async function parse() {
-    const response = await cohere.generate({
-        model: 'xlarge',
-        prompt: 'Buying a stock is easy, but buying the right stock without a time-tested strategy is incredibly hard. So what are the best stocks to buy now or put on a watchlist?\n\nTLDR:',
-        max_tokens: 10,
-        temperature: 0.5,
-        k: 0,
-        p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-        stop_sequences: ["--"],
-        return_likelihoods: 'NONE'
-      })
-    return response.body.generations[0].text
-}
+// async function parse() {
+//     const response = await cohere.generate({
+//         model: 'xlarge',
+//         prompt: 'Buying a stock is easy, but buying the right stock without a time-tested strategy is incredibly hard. So what are the best stocks to buy now or put on a watchlist?\n\nTLDR:',
+//         max_tokens: 10,
+//         temperature: 0.5,
+//         k: 0,
+//         p: 1,
+//         frequency_penalty: 0,
+//         presence_penalty: 0,
+//         stop_sequences: ["--"],
+//         return_likelihoods: 'NONE'
+//       })
+//     return response.body.generations[0].text
+// }
 
-async function start() {
-    const wait = await parse()
-    return wait
-}
+// async function start() {
+//     const wait = await parse()
+//     return wait
+// }
 
 // Data to load
 let investing_scroll = [
@@ -63,6 +63,6 @@ let investing_scroll = [
     }
 ]
 
-(async () => {
-    investing_scroll[1]["description"] = await start();
-})()
+// (async () => {
+//     investing_scroll[1]["description"] = await start();
+// })()
